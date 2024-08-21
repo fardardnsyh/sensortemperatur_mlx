c51d38e6ea24a8d6b0c87add5277952a2dd45f8e
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-21 07:48:50 +0700

    Files upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Settings from "./pages/Settings";
+import Questions from "./pages/Questions";
+import FinalScreen from "./pages/FinalScreen";
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+function App() {
+  return (
+    <Router>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Switch>
+            <Route path="/" exact>
+              <Typography variant="h2" fontWeight="bold">
+                Quiz App
+              </Typography>
+              <Settings />
+            </Route>
+            <Route path="/questions">
+              <Questions />
+            </Route>
+            <Route path="/score">
+              <FinalScreen />
+            </Route>
+          </Switch>
+        </Box>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
